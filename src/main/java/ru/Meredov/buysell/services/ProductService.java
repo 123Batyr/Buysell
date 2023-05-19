7a5ade8d60ddfcada78b5893d66454d251d59347
BatyrM <meredovbatyr2001@gmail.com>
2023-05-19 20:12:47 +0500

    Progress 2 - Подключение к PostgreSQL

--- src/main/java/ru/Meredov/buysell/services/ProductService.java
+++ src/main/java/ru/Meredov/buysell/services/ProductService.java
@@ -1,39 +1,36 @@
 package ru.Meredov.buysell.services;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.Meredov.buysell.models.Product;
+import ru.Meredov.buysell.repositories.ProductRepository;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class ProductService {
-    private List<Product> products = new ArrayList<>();
-    private long ID = 0;
+    private final ProductRepository productRepository;
 
-    {
-        products.add(new Product(++ID,"PlayStation 5", "Simple descripton", 67000, "Ekaterinburg", "tomas"));
-        products.add(new Product(++ID,"Iphone 8", "Simple descripton", 24000, "Moscow", "Egor"));
 
-    }
-
-    public List<Product> listProducts() {
-        return products;
+    public List<Product> listProducts(String title) {
+        if (title != null) return productRepository.findByTitle(title);
+        return productRepository.findAll();
     }
 
     public void saveProduct(Product product) {
-        product.setId(++ID);
-        products.add(product);
+        log.info("Saving new {}", product);
+        productRepository.save(product);
     }
 
     public void deleteProduct(Long id) {
-        products.removeIf(product -> product.getId().equals(id));
+        productRepository.deleteById(id);
     }
 
     public Product getProductById(Long id) {
-        for (Product product : products) {
-            if (product.getId().equals(id)) return product;
-        }
-        return null;
+        return productRepository.findById(id).orElse(null);
     }
 }
